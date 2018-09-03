211283bfba6d9d3bbec821a51c55a589fcd8ad63
VicChern <32546828+VicChern@users.noreply.github.com>
2018-09-03 17:55:08 +0300

    Show name of the day when have some stdin

--- Solution.java
+++ Solution.java
@@ -1,80 +1,23 @@
-package com.company;
+package com;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
-class Solution {
+public class Solution {
+
+    public static Object getDay (String day,String month,String year){
+        Calendar cal = Calendar.getInstance();
+        cal.set(Integer.valueOf(day),Integer.valueOf(month),Integer.valueOf(year));
+         return cal.get(Calendar.DAY_OF_WEEK);
+
+    }
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String firstName = scan.next();
-        String lastName = scan.next();
-        int id = scan.nextInt();
-        int numScores = scan.nextInt();
-        int[] testScores = new int[numScores];
-        for (int i = 0; i < numScores; i++) {
-            testScores[i] = scan.nextInt();
-        }
-        scan.close();
+        Scanner in = new Scanner(System.in);
+        String month = in.next();
+        String day = in.next();
+        String year = in.next();
 
-        Student s = new Student(firstName, lastName, id, testScores);
-        s.printPerson();
-        System.out.println("Grade: " + s.calculate());
+        System.out.println(getDay(day, month, year));
     }
 }
 
-class Person {
-    protected String firstName;
-    protected String lastName;
-    protected int idNumber;
 
-    // Constructor
-    Person(String firstName, String lastName, int identification) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.idNumber = identification;
-    }
-
-    // Print person data
-    public void printPerson() {
-        System.out.println(
-                "Name: " + lastName + ", " + firstName
-                        + "\nID: " + idNumber);
-    }
-
-}
-
-class Student extends Person {
-    private int[] testScores;
-    int sum;
-    int average;
-    public Student(String firstName, String lastName, int identification, int[] testScores) {
-        super(firstName, lastName, identification);
-        this.testScores = testScores;
-    }
-
-    char calculate() {
-        for (int i= 0; i < testScores.length; i++) {
-            sum += testScores[i];
-        }
-        average = sum / testScores.length;
-        if (average <= 100 && average >= 90 )
-            return  'O';
-        if (average < 90 && average >= 80 )
-            return  'E';
-        if (average < 80 && average >= 70 )
-            return  'A';
-        if (average < 70 && average >= 55 )
-            return  'P';
-        if (average < 55 && average >= 40 )
-            return  'D';
-        if (average < 40 )
-            return  'T';
-        return 'z';
-    }
-
-}
