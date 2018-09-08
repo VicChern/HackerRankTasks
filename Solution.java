65fe5570172bdf2782e46336f01ea5639683f0ec
VicChern <32546828+VicChern@users.noreply.github.com>
2018-09-08 16:37:49 +0300

    Nested logic (Library example)

--- Solution.java
+++ Solution.java
@@ -1,23 +1,39 @@
-package com;
-
+package com.company;
 import java.util.*;
 
 public class Solution {
+    static Scanner scanner = new Scanner(System.in);
+    static int Day;
+    static int Month;
+    static int Year;
 
-    public static Object getDay (String day,String month,String year){
-        Calendar cal = Calendar.getInstance();
-        cal.set(Integer.valueOf(day),Integer.valueOf(month),Integer.valueOf(year));
-         return cal.get(Calendar.DAY_OF_WEEK);
+
+    public static void main(String[] args) {
+        Day = scanner.nextInt();
+        Month = scanner.nextInt();
+        Year = scanner.nextInt();
+        System.out.println(getCurrentDate(Day, Month, Year));
 
     }
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String month = in.next();
-        String day = in.next();
-        String year = in.next();
 
-        System.out.println(getDay(day, month, year));
+    public static int getCurrentDate(int D, int M, int Y) {
+        D = scanner.nextInt();
+        M = scanner.nextInt();
+        Y = scanner.nextInt();
+
+        if ((Day <= D) && (Month <= M) && (Y <= Year)) {
+            return 0;
+        }
+
+        if ((Day != D) && (Month == M) && (Year == Y)) {
+            return 15 * (Day - D);
+        }
+        if ((Month != M) && (Year == Y)) {
+            return 500 * (Month - M);
+        }
+        if (Y < Year) {
+            return 10000;
+        }
+        return 0;
     }
 }
-
-
